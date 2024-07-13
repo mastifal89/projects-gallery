25a16c80549ecc303c3ae3e9f2ea03bb7004c3f4
Juan Manuel <juanrata89@gmail.com>
2024-07-13 10:52:48 -0300

    Added interests and education

--- src/app/(dashboard)/about-me/page.tsx
+++ src/app/(dashboard)/about-me/page.tsx
@@ -9,23 +9,48 @@ import Image from "next/image";
 const personalInfo = [
   {
     title: "bio",
-    content: `/**
- *
- * About me
- * I have 5 years of experience in web development lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
- *
- * Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat officia deserunt mollit anim id est laborum.
- */`,
+    content: `/*
+About me
+I have 5 years of experience in web development 
+lorem ipsum dolor sit amet, 
+consectetur adipiscing elit, 
+sed do eiusmod tempor 
+incididunt ut labore et dolore magna aliqua. 
+Ut enim ad minim veniam, quis nostrud exercitation 
+ullamco laboris nisi ut 
+aliquip ex ea commodo consequat.
+ 
+Duis aute irure dolor 
+in reprehenderit in voluptate 
+velit esse cillum dolore 
+eu fugiat nulla pariatur. 
+Excepteur sint occaecat 
+officia deserunt mollit anim 
+id est laborum.
+/`,
     icon: "/icons/folder1.svg",
   },
   {
     title: "interests",
-    content: "asdasdasdasdasd",
+    content: `/*
+ Interests
+ - Web development
+ - Open source contributions
+ - Artificial Intelligence and Machine Learning
+ - Reading tech blogs and articles
+ - Hiking and outdoor activities
+ - Playing the guitar
+ */`,
     icon: "/icons/folder2.svg",
   },
   {
     title: "education",
-    content: "asdasdasdasdasd",
+    content: `/*
+ Education
+ - B.Sc. in Computer Science from University X (2015-2019)
+ - M.Sc. in Software Engineering from University Y (2019-2021)
+ - Various online courses and certifications in web development and AI
+ */`,
     icon: "/icons/folder3.svg",
   },
 ];
@@ -57,23 +82,51 @@ export default function AboutMe() {
     setSection(section);
   };
 
+  const formatContent = (content: any) => {
+    return content
+      .split("\n")
+      .map((line: any) => `* ${line}`)
+      .join("\n");
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.aboutMeMenu}>
         <section className={styles.sections}>
           <div className={styles.sectionIcon}>
-            <Image height={25} width={25} alt="image" src="/icons/info-professional.svg" />
+            <Image
+              height={25}
+              width={25}
+              alt="image"
+              src="/icons/info-professional.svg"
+            />
           </div>
           <div className={styles.sectionIcon}>
-            <Image height={25} width={25} alt="image" src="/icons/info-personal.svg" />
+            <Image
+              height={25}
+              width={25}
+              alt="image"
+              src="/icons/info-personal.svg"
+            />
           </div>
           <div className={styles.sectionIcon}>
-            <Image height={25} width={25} alt="image" src="/icons/info-hobbies.svg" />
+            <Image
+              height={25}
+              width={25}
+              alt="image"
+              src="/icons/info-hobbies.svg"
+            />
           </div>
         </section>
         <section className={styles.sectionContent}>
           <div className={styles.sectionTitle}>
-            <Image height={10} width={10} alt="image" className={styles.arrow} src="/icons/arrow.svg" />
+            <Image
+              height={10}
+              width={10}
+              alt="image"
+              className={styles.arrow}
+              src="/icons/arrow.svg"
+            />
             <p className={styles.sectionPersonalInfo}>personal-info</p>
           </div>
           {personalInfo.map((info, index) => (
@@ -83,25 +136,55 @@ export default function AboutMe() {
               onClick={() => handleSectionClick(index)}
             >
               <div className={styles.item}>
-                <Image height={15} width={15} alt="image" className={styles.diple} src="/icons/diple.svg" />
-                <Image height={15} width={15} alt="image" className={styles.folder} src={info.icon} />
+                <Image
+                  height={15}
+                  width={15}
+                  alt="image"
+                  className={styles.diple}
+                  src="/icons/diple.svg"
+                />
+                <Image
+                  height={15}
+                  width={15}
+                  alt="image"
+                  className={styles.folder}
+                  src={info.icon}
+                />
                 <p style={{ margin: 0 }}>{info.title}</p>
               </div>
             </div>
           ))}
           <div className={styles.sectionContact}>
-            <Image height={10} width={10} alt="image" className={styles.arrow} src="/icons/arrow.svg" />
+            <Image
+              height={10}
+              width={10}
+              alt="image"
+              className={styles.arrow}
+              src="/icons/arrow.svg"
+            />
             <p className={styles.sectionPersonalInfo}>contacts</p>
           </div>
           <div className={styles.contactSources}>
             <div className={styles.contactSourcesItem}>
-              <Image height={15} width={15} alt="image" className={styles.sourceItemsImage} src="/icons/email.svg" />
+              <Image
+                height={15}
+                width={15}
+                alt="image"
+                className={styles.sourceItemsImage}
+                src="/icons/email.svg"
+              />
               <a className={styles.contactSourcesText} href="/">
                 juanrata89@gmail.com
               </a>
             </div>
             <div className={styles.contactSourcesItem}>
-              <Image height={15} width={15} alt="image" className={styles.sourceItemsImage} src="/icons/phone.svg" />
+              <Image
+                height={15}
+                width={15}
+                alt="image"
+                className={styles.sourceItemsImage}
+                src="/icons/phone.svg"
+              />
               <a className={styles.contactSourcesText} href="/">
                 +543525487411
               </a>
@@ -114,15 +197,23 @@ export default function AboutMe() {
           <div className={styles.bodyTab}>
             <div className={styles.tab}>
               <p className={styles.personalInfoText}>personal-info</p>
-              <Image height={15} width={15} alt="image" className={styles.personalInfoImg} src="/icons/close.svg" />
+              <Image
+                height={15}
+                width={15}
+                alt="image"
+                className={styles.personalInfoImg}
+                src="/icons/close.svg"
+              />
             </div>
           </div>
           <div className={styles.bodyTextContainer}>
-              <pre className={styles.bodyText}>
-                  <code className="language-javascript">
-                    <span className={styles.textContent}>{personalInfo[section].content}</span>
-                  </code>
-              </pre>
+            <pre className={styles.bodyText}>
+              <code className="language-javascript">
+                <span className={styles.textContent}>
+                  {formatContent(personalInfo[section].content)}
+                </span>
+              </code>
+            </pre>
             <div className={styles.scrollBar}>
               <div className={styles.scroll}></div>
             </div>
@@ -132,13 +223,18 @@ export default function AboutMe() {
           <div className={styles.bodyTab}></div>
           <div className={styles.snippetContainer}>
             <div className={styles.snippetSubContainer}>
-              <h3 className={styles.snippetTitle}>{`// Code snippet showcase`}</h3>
+              <h3
+                className={styles.snippetTitle}
+              >{`// Code snippet showcase`}</h3>
               <div className={styles.snippets}>
                 {codeSnippets.map((snippet, index) => (
                   <div key={index} className={styles.snippet}>
                     <div className={styles.snippetHeader}>
                       <div style={{ display: "flex" }}>
-                        <Image height={15} width={15} alt="image"
+                        <Image
+                          height={15}
+                          width={15}
+                          alt="image"
                           src="/images/1551846149745.jfif"
                           className={styles.snippetImage}
                         />
@@ -158,14 +254,20 @@ export default function AboutMe() {
                       </div>
                       <div className={styles.snippetDetailsStarsContainer}>
                         <div className={styles.snippetDetails}>
-                          <Image height={15} width={15} alt="image"
+                          <Image
+                            height={15}
+                            width={15}
+                            alt="image"
                             src="/icons/comments.svg"
                             className={styles.snippetDetailsIcon}
                           />
                           <span>details</span>
                         </div>
                         <div className={styles.snippetStars}>
-                          <Image height={15} width={15} alt="image"
+                          <Image
+                            height={15}
+                            width={15}
+                            alt="image"
                             src="/icons/star.svg"
                             className={styles.snippetStarsIcon}
                           />
